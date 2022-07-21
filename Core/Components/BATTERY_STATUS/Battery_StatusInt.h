e1870dc7b93096d3a0f0f89d5e35319a178ddf56
FedAlt <strambo99@gmail.com>
2022-07-21 16:28:43 +0200

    Battery Status Internal header file

---
+++ Core/Components/BATTERY_STATUS/Battery_StatusInt.h
@@ -0,0 +1,16 @@
+/*
+ * Battery_StatusInt.h
+ *
+ *  Created on: 21 lug 2022
+ *      Author: Federico Altieri
+ */
+
+#ifndef COMPONENTS_BATTERY_STATUS_BATTERY_STATUSINT_H_
+#define COMPONENTS_BATTERY_STATUS_BATTERY_STATUSINT_H_
+
+#include "Battery_StatusExt.h"
+
+#define VREF 3.3 /* FIXME: This value have to be measured !!! */
+
+
+#endif /* COMPONENTS_BATTERY_STATUS_BATTERY_STATUSINT_H_ */
