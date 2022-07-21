cba07af4632f7919feb9e664dc7a5f0af33ae045
FedAlt <strambo99@gmail.com>
2022-07-21 16:29:00 +0200

    Battery Status External header file

---
+++ Core/Components/BATTERY_STATUS/Battery_StatusExt.h
@@ -0,0 +1,15 @@
+/*
+ * Battery_StatusExt.h
+ *
+ *  Created on: 21 lug 2022
+ *      Author: Federico Altieri
+ */
+
+#ifndef COMPONENTS_BATTERY_STATUS_BATTERY_STATUSEXT_H_
+#define COMPONENTS_BATTERY_STATUS_BATTERY_STATUSEXT_H_
+
+/* Public Functions */
+
+HAL_StatusTypeDef get_Battery_Status (ADC_HandleTypeDef hadc, float *stat);
+
+#endif /* COMPONENTS_BATTERY_STATUS_BATTERY_STATUSEXT_H_ */
