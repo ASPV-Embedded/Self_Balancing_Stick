7a5e81008eec13260a330dba3b59d49dcdc6f6e3
Emanuele Somma <emanuele.somma98@gmail.com>
2024-01-21 19:08:25 +0100

    [develop] Corretta define su Prescaler usato su timer Motori

--- Core/Components/MOTOR/MotorInt.h
+++ Core/Components/MOTOR/MotorInt.h
@@ -10,9 +10,7 @@
 
 #include "MotorExt.h"
 
-#define PWM_FREQUENCY_KHZ 		   25			/* f_in = [20, 30] KHz */
-
 #define ARR_VALUE 	     (uint16_t)1000			/* Value of the ARR Register for source clock frequency 84MHz*/
-#define PSC_VALUE 				   1			/* Value of the PSC Register */
+#define PSC_VALUE 				   2			/* Value of the PSC Register */
 
 #endif /* __MOTORINT_H_ */
